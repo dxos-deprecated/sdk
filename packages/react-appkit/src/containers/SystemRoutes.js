3d212cad3bc2c41b57ec6c8c240d1962d364950e
Max Fierro <elmasse@gmail.com>
2020-06-30 11:54:34 -0300

    clean up react-appkit (#9)
    * Remove unused components.
    
    * Remove unused components.
    
    * Put back MemberTable.

--- packages/react-appkit/src/containers/SystemRoutes.js
+++ packages/react-appkit/src/containers/SystemRoutes.js
@@ -6,9 +6,6 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Authenticator from './Authenticator';
-import FeedStore from './FeedStore';
-import FeedViewer from './FeedViewer';
-import KeyRing from './KeyRing';
 import Members from './Members';
 
 /**
@@ -21,10 +18,7 @@ const SystemRoutes = (router) => {
   return (
     <Route exact path={[routes.auth, routes.keys, routes.store, routes.feed, routes.members]}>
       <Route exact path={routes.auth} component={Authenticator} />
-      <Route exact path={routes.keys} component={KeyRing} />
       <Route exact path={routes.members} component={Members} />
-      <Route exact path={routes.store} component={FeedStore} />
-      <Route exact path={routes.feed} component={FeedViewer} />
     </Route>
   );
 };
