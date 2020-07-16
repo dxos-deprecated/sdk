39bc2f7ce83de883ebcb2f604a4628efc4f87d89
Dmytro Maretskyi <maretskii@gmail.com>
2020-07-16 11:48:04 +0300

    Extract common logic to environment class

---
+++ testing/spawn-testing/src/environment.js
@@ -0,0 +1,94 @@
+import debug from 'debug';
+import { Broker } from "./broker";
+import dequal from 'dequal';
+
+const log = debug('dxos:spawn-testing:environment');
+
+export class Environment {
+  constructor() {
+    this._broker = new Broker();
+  }
+
+  async start() {
+    await this._broker.createSignal();
+  }
+
+  async addPeers({ count, ...opts}) {
+    for (let i = 0; i < (count || 1); i++) {
+      await this._addAndInvitePeer(opts);
+    }
+  }
+
+  async _addAndInvitePeer(opts) {
+    const prevPeer = this._broker.peers[this._broker.peers.length - 1];
+    const peer = await this._broker.createPeer(opts);
+    if(!prevPeer) {
+      const { publicKey } = await peer.call('createParty');
+      log('> party created', publicKey.toString('hex'));
+      this._partyKey = publicKey;
+    } else {
+      const invitation = await prevPeer.call('createInvitation', { publicKey: this._partyKey });
+      log('> invitation created');
+
+      await peer.call('joinParty', { invitation });
+      log(`> peer joined to the party`);
+    }
+    await peer.call('initAgent');
+  }
+
+  async runTicks({ count = 1, delay = 10 } = {}) {
+    for(let i = 0; i < count; i++) {
+      for (const peer of this._broker.peers) {
+        await peer.call('tick');
+      }
+      await sleep(delay);
+    }
+  }
+
+  async comparePeerStates() {
+    const modelObjects = await Promise.all(this._broker.peers.map(peer => peer.call('getModelObjects')));
+    return modelObjects[0].length !== 0 && arrayItemsEqual(modelObjects, compareModelStates);
+  }
+
+  waitForSync() {
+    return new Promise(resolve => {
+      const check = async () => {
+        if (await this.comparePeerStates()) {
+          resolve();
+        }
+      }
+  
+      const peerStates = new Map();
+      for (const [key, peer] of this._broker._peers) {
+        peer.on('model-update', async ({ state }) => {
+          peerStates.set(key, state);
+          const states = Array.from(peerStates.values());
+          if (states.length === this._broker.peers.length && arrayItemsEqual(states, (a, b) => a.objectCount === b.objectCount)) {
+            check();
+          }
+        });
+      }
+  
+      check();
+    });
+  }
+
+  async destroy() {
+    await this._broker.destroy();
+  }
+}
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+function compareModelStates (stateA, stateB) {
+  if (stateA.length !== stateB.length) return false;
+  return stateA.every(a => stateB.some(b => a.id === b.id && dequal(a, b)));
+}
+
+/**
+ * @returns true if all items in the array are equal
+ */
+function arrayItemsEqual (arr, cmp) {
+  if (arr.length <= 1) return true;
+  return arr.slice(1).every(x => cmp(arr[0], x));
+}
