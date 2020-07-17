9c5d05e7a5b93f14e8e05c7184e21104e5d2a7e0
Dmytro Maretskyi <maretskii@gmail.com>
2020-07-17 13:38:58 +0300

    Add throughput test

---
+++ testing/spawn-testing/src/tests/throughput.js
@@ -0,0 +1,48 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+const debug = require('debug');
+const mri = require('mri');
+const { basename } = require('path');
+
+const { Environment } = require('../environment');
+
+const log = debug('dxos:spawn-testing:example');
+
+async function run ({ readers = 1, ticks = 60, delay = 1000, ...opts } = {}) {
+
+  const environment = new Environment();
+  await environment.start();
+  await environment.addPeers(opts);
+
+  await environment.addPeers({
+    ...opts,
+    count: readers,
+    agent: './src/agents/reading-agent.js',
+  });
+
+  log('> network full connected');
+
+  log('> sync started');
+  console.time('sync');
+
+  await environment.writeMetrics(`./metrics-${basename(__filename)}.log`);
+
+  await environment.runTicks({ count: ticks, delay });
+  log('> finished creating items');
+
+  await environment.waitForSync();
+
+  if (!await environment.comparePeerStates()) {
+    log('> state mismatch');
+    process.exit(-1);
+  }
+
+  log('> sync successful');
+  console.timeEnd('sync');
+
+  await environment.destroy();
+}
+
+run(mri(process.argv.slice(2)));
