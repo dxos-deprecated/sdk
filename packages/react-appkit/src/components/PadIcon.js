54c1c5169fcb2cf78b3a54e3f0a319f5c6619415
Rich Burdon <rich.burdon@gmail.com>
2020-07-17 05:05:14 -0400

    Restructure parties. (#74)

--- packages/react-appkit/src/components/PadIcon.js
+++ packages/react-appkit/src/components/PadIcon.js
@@ -4,12 +4,15 @@
 
 import React from 'react';
 
+import AppIcon from '@material-ui/icons/Apps';
+
 import { usePads } from '../hooks';
 
 const PadIcon = ({ type }) => {
   const [pads] = usePads();
+
   const pad = pads.find(pad => pad.type === type);
-  return pad ? <pad.icon /> : null;
+  return pad ? <pad.icon /> : <AppIcon />;
 };
 
 export default PadIcon;
