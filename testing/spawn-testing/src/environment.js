3674f2ae1f3e13528f7fdcfad5e872fa40869903
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2020-07-20 18:03:09 +0300

    Use model append stats for sync check (#86)
    * Use model stats for sync check
    
    * Add anchor API
    
    * Lint
    
    * Lower max size to 1k

--- testing/spawn-testing/src/environment.js
+++ testing/spawn-testing/src/environment.js
@@ -57,11 +57,25 @@ export class Environment extends EventEmitter {
     return modelObjects[0].length !== 0 && arrayItemsEqual(modelObjects, compareModelStates);
   }
 
-  waitForSync () {
+  async areModelsReplicated () {
+    const states = await Promise.all(this._broker.peers.map(peer => peer.call('getState')));
+    const modelStates = states.map(state => Object.values(state.agent.models)[0]);
+    const totalAppended = modelStates.reduce((acc, state) => acc + state.appended, 0);
+    if (!modelStates.every(state => state.updated === totalAppended)) {
+      return false;
+    }
+    return { totalAppended };
+  }
+
+  waitForSync (anchor = undefined) {
     return new Promise(resolve => {
       const check = async () => {
-        if (await this.comparePeerStates()) {
-          resolve();
+        const currentState = await this.areModelsReplicated();
+        if (currentState) {
+          if (anchor && currentState.totalAppended <= anchor) {
+            return;
+          }
+          resolve(currentState);
         }
       };
 
@@ -70,7 +84,8 @@ export class Environment extends EventEmitter {
         peer.on('model-update', async ({ state }) => {
           peerStates.set(key, state);
           const states = Array.from(peerStates.values());
-          if (states.length === this._broker.peers.length && arrayItemsEqual(states, (a, b) => a.objectCount === b.objectCount)) {
+          const totalAppended = states.reduce((acc, state) => acc + state.appended, 0);
+          if (states.length === this._broker.peers.length && states.every(state => state.updated === totalAppended)) {
             check();
           }
         });
