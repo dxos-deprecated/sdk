293764787c571037ec4f8cb17cbf3efcbd591545
Rich Burdon <rich.burdon@gmail.com>
2020-07-20 10:50:41 -0400

    Attempt to have full-stack stories. (#82)
    * Attempt to have full-stack stories.
    
    * useClient working.
    
    * Remove comments.

--- packages/react-appkit/stories/app.stories.js
+++ packages/react-appkit/stories/app.stories.js
@@ -4,33 +4,54 @@
 
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { storiesOf } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
+import { storiesOf } from '@storybook/react';
 
+import Box from '@material-ui/core/Box';
+
+import { keyToString } from '@dxos/crypto';
 import { ErrorHandler } from '@dxos/debug';
 import { ClientContextProvider, useClient } from '@dxos/react-client';
 
 import { AppKitContextProvider } from '../src';
 
-// TODO(burdon): Goal to test context provider (currently doesn't work).
-// TODO(burdon): Dummy client (in-memory).
-const config = {};
+const config = {
+  client: {
+    keyStorage: {
+      type: 'ram'
+    },
+
+    feedStorage: {
+      root: './echo/feeds',
+      type: 'ram'
+    }
+  },
+  debug: {
+    mode: 'development'
+  }
+};
 
 const initialState = {};
 
 const Test = () => {
-  // TODO(burdon): useClient returns null initially.
   const client = useClient();
-  console.log(client);
+
+  // TODO(burdon): ReferenceError: Cannot access before initialization.
+  // const parties = useParties();
+
+  const keys = client.keyring.keys;
 
   return (
-    <div>
-      <pre>{client && client.config}</pre>
-    </div>
+    <Box m={2}>
+      <h1>Keys</h1>
+      {keys.map(key => (
+        <div key={key.publicKey}>{keyToString(key.publicKey)}</div>
+      ))}
+    </Box>
   );
 };
 
-storiesOf('AppKit', module)
+storiesOf('Appkit', module)
 
   // TODO(burdon): Not working?
   .addDecorator(StoryRouter())
