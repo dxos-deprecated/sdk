3b49838b17af9219c6d1dd7ba7086e2142766b26
rzadp <przemek@ethworks.io>
2020-07-22 16:11:39 +0200

    Change tree select color

--- packages/react-appkit/src/components/PartyTree.js
+++ packages/react-appkit/src/components/PartyTree.js
@@ -6,7 +6,7 @@ import clsx from 'clsx';
 import React, { useState, useEffect, useCallback, forwardRef } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { blue, green } from '@material-ui/core/colors';
+import { grey, green } from '@material-ui/core/colors';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TreeView from '@material-ui/lab/TreeView';
@@ -31,7 +31,7 @@ const treeItemBaseStyles = theme => ({
     },
 
     '&$selected > $content': {
-      backgroundColor: `var(--tree-view-bg-color, ${blue[400]})`,
+      backgroundColor: `var(--tree-view-bg-color, ${grey[400]})`,
       color: 'white'
     },
 
