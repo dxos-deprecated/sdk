32398a5ba9a99641d11fdb675c2e5af7e10e0ccf
Max Fierro <elmasse@gmail.com>
2020-07-24 10:46:05 -0300

    client api refactor: joinParty, admitDevice, createSubscription (#100)
    * - Depreacate warning on createClient().
    - Add createSubscription().
    
    * use client.createSubscription instead of modelFactory.createModel.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * - Depreacate warning on createClient().
    - Add createSubscription().
    
    * use client.createSubscription instead of modelFactory.createModel.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * - Depreacate warning on createClient().
    - Add createSubscription().
    
    * use client.createSubscription instead of modelFactory.createModel.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * - Depreacate warning on createClient().
    - Add createSubscription().
    
    * use client.createSubscription instead of modelFactory.createModel.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * deprecate ClientContextProvider.
    
    * New ClientProvider.
    
    * client api: joinParty, admitDevice.
    
    * Use client joinParty and admitDevice.
    
    * Update NOTES.
    
    * Simplify code in authenticator useEffect.
    
    * Fix createSubscription signature.
    
    * - Depreacate warning on createClient().
    - Add createSubscription().
    
    * use client.createSubscription instead of modelFactory.createModel.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * Update NOTES
    
    * Add reset() to lifecycle methods.
    
    * deprecate ClientContextProvider.
    
    * New ClientProvider.
    
    * client api: joinParty, admitDevice.
    
    * Use client joinParty and admitDevice.
    
    * Update NOTES.
    
    * Simplify code in authenticator useEffect.
    
    * Fix createSubscription signature.
    
    * Fix imports.
    
    * Fix PR comments.
    
    * Add documentation.

---
+++ packages/react-client/src/containers/ClientProvider.js
@@ -0,0 +1,51 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import React, { useEffect, useState } from 'react';
+
+import { logs } from '@dxos/debug';
+import metrics from '@dxos/metrics';
+
+import { ClientContext } from '../hooks/context';
+
+const { error } = logs('react-client:ClientProvider');
+
+/**
+ * Client provider container.
+ */
+const ClientProvider = ({ client, config = {}, children }) => {
+  const [initError, setInitError] = useState(false);
+
+  useEffect(() => {
+    let mounted = true;
+
+    (async () => {
+      if (mounted) {
+        try {
+          await client.initialize();
+
+          // Console access.
+          if (config.devtools) {
+            window.__DXOS__ = { client, metrics };
+          }
+        } catch (err) {
+          error(err);
+          setInitError(err);
+        }
+      }
+    })();
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  return (
+    <ClientContext.Provider value={{ config, client, reset: () => client.reset(), initError }}>
+      {children}
+    </ClientContext.Provider>
+  );
+};
+
+export default ClientProvider;
--- packages/react-client/src/hooks/authenticator.js
+++ packages/react-client/src/hooks/authenticator.js
@@ -5,9 +5,7 @@
 import { useEffect, useState } from 'react';
 
 import { trigger } from '@dxos/async';
-import { createAuthMessage, codec } from '@dxos/credentials';
 import { keyToString } from '@dxos/crypto';
-import { InviteType } from '@dxos/party-manager';
 
 import { useClient } from './client';
 
@@ -22,48 +20,22 @@ export const useAuthenticator = (invitation) => {
   const [secretProvider, secretResolver] = trigger();
 
   useEffect(() => {
-    // An invitation where we can use our Identity key for auth.
-    if (InviteType.OFFLINE_KEY === invitation.type) {
-      // Connect to inviting peer.
-      client.partyManager.joinParty(invitation, (info) =>
-        codec.encode(createAuthMessage(client.keyring, info.id.value,
-          client.partyManager.identityManager.keyRecord,
-          client.partyManager.identityManager.deviceManager.keyChain,
-          null, info.authNonce.value))
-      )
-        .then(party => {
-          setState({ topic: keyToString(party.publicKey) });
-        })
-
-        // TODO(burdon): Doesn't support retry. Provide hint (e.g., should retry/cancel).
-        .catch(err => {
-          setState({ error: String(err) });
-        });
-    } else if (invitation.identityKey) {
-      // An invitation for this device to join an existing Identity.
-      // Join the Identity
-      client.partyManager.identityManager.deviceManager.admitDevice(invitation, secretProvider)
-        .then(() => {
-          setState({ identity: keyToString(invitation.identityKey) });
-        })
-
-        // TODO(burdon): Doesn't support retry. Provide hint (e.g., should retry/cancel).
-        .catch(err => {
-          setState({ error: String(err) });
-        });
-    // An invitation for this Identity to join a Party.
-    } else {
-      // Connect to inviting peer.
-      client.partyManager.joinParty(invitation, secretProvider)
-        .then(party => {
-          setState({ topic: keyToString(party.publicKey) });
-        })
-
-        // TODO(burdon): Doesn't support retry. Provide hint (e.g., should retry/cancel).
-        .catch(err => {
-          setState({ error: String(err) });
-        });
+    async function runEffect () {
+      if (invitation.identityKey) {
+        // An invitation for this device to join an existing Identity.
+        // Join the Identity
+        await client.admitDevice(invitation, secretProvider);
+        setState({ identity: keyToString(invitation.identityKey) });
+      } else {
+        const party = await client.joinParty(invitation, secretProvider);
+        setState({ topic: keyToString(party.publicKey) });
+      }
     }
+
+    runEffect().catch(err => {
+      // TODO(burdon): Doesn't support retry. Provide hint (e.g., should retry/cancel).
+      setState({ error: String(err) });
+    });
   }, []);
 
   return [state, secretResolver];
