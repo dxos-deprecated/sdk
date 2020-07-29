c99338f42c0d5457ff4ab903624b8d355013c7f8
jakub_jozwiak <kubencki@gmail.com>
2020-07-29 15:27:10 +0200

    Set max height of member list and handle long names

--- packages/react-appkit/src/components/MemberList.js
+++ packages/react-appkit/src/components/MemberList.js
@@ -11,6 +11,10 @@ import { humanize } from '@dxos/crypto';
 import { MemberAvatar } from '.';
 
 const useStyles = makeStyles(() => ({
+  membersList: {
+    maxHeight: 360,
+    overflow: 'auto'
+  },
   member: {
     display: 'flex',
     justifyContent: 'space-between',
@@ -22,11 +26,18 @@ export const MemberList = ({ party }) => {
   const sorter = (a, b) => (a.displayName < b.displayName ? -1 : a.displayName > b.displayName ? 1 : a.isMe ? -1 : 1);
   const classes = useStyles();
 
+  const shortenName = name => {
+    if (name.length > 20) {
+      return name.substring(0, 18) + '...';
+    }
+    return name;
+  };
+
   return (
-    <List>
+    <List className={classes.membersList}>
       {party.members.sort(sorter).map((member) => (
         <ListItem key={member.publicKey} className={classes.member}>
-          {member.displayName || humanize(member.publicKey)}
+          {shortenName(member.displayName || humanize(member.publicKey))}
           <MemberAvatar member={member} />
         </ListItem>
       ))}
