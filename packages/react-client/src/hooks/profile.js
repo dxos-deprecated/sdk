757a176b17b60554059584d385c66d0989c88d5a
Max Fierro <elmasse@gmail.com>
2020-08-05 09:30:02 -0300

    Use client.getProfile in useProfile hook.

--- packages/react-client/src/hooks/profile.js
+++ packages/react-client/src/hooks/profile.js
@@ -2,37 +2,22 @@
 // Copyright 2020 DXOS.org
 //
 
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import getDisplayName from 'react-display-name';
 
 import { onEvent } from '@dxos/async';
-import { keyToString } from '@dxos/crypto';
 
 import { useClient } from './client';
 
 export const useProfile = () => {
   const client = useClient();
-
-  const getProfileData = useCallback(() => {
-    const { identityManager: idm } = client.partyManager;
-
-    if (!idm || !idm.publicKey) return;
-
-    const publicKey = keyToString(idm.publicKey);
-
-    return {
-      username: idm.displayName || publicKey,
-      publicKey
-    };
-  }, [client]);
-
-  const [profile, setProfile] = useState(getProfileData());
+  const [profile, setProfile] = useState(client.getProfile());
 
   useEffect(() => onEvent(
     client.partyManager.identityManager,
     'update',
-    () => setProfile(getProfileData())
-  ), [client, getProfileData]);
+    () => setProfile(client.getProfile())
+  ), [client]);
 
   return profile;
 };
