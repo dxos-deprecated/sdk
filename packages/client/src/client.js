7e642e0ea99b15639e0d733481a09199617b22a6
Max Fierro <elmasse@gmail.com>
2020-08-10 08:33:05 -0300

    Invite to party fixes. (#141)
    * Fix useAuthenticator hook.
    
    * Added encode/decode Invitation methods to client. Hide InvitationDescriptor usage.

--- packages/client/src/client.js
+++ packages/client/src/client.js
@@ -15,7 +15,7 @@ import { FeedStore } from '@dxos/feed-store';
 import metrics from '@dxos/metrics';
 import { ModelFactory } from '@dxos/model-factory';
 import { NetworkManager, SwarmProvider } from '@dxos/network-manager';
-import { PartyManager, InviteType } from '@dxos/party-manager';
+import { PartyManager, InviteType, InviteDetails, InvitationDescriptor } from '@dxos/party-manager';
 
 import { defaultClientConfig } from './config';
 
@@ -158,6 +158,24 @@ export class Client {
   }
 
   /**
+   *
+   * @param {Buffer} publicKey Party publicKey
+   * @param {SecretProvider} secretProvider
+   * @param {Object} options
+   * @param {Object} options.onFinish
+   */
+  async inviteToParty (publicKey, secretProvider, options = {}) {
+    return this._partyManager.inviteToParty(
+      publicKey,
+      new InviteDetails(InviteType.INTERACTIVE, {
+        secretValidator: (invitation, secret) => secret && secret.equals(invitation.secret),
+        secretProvider
+      }),
+      options
+    );
+  }
+
+  /**
    * Join a Party by redeeming an Invitation.
    * @param {InvitationDescriptor} invitation
    * @param {SecretProvider} secretProvider
@@ -192,6 +210,23 @@ export class Client {
       // Join the Identity
       return this._partyManager.identityManager.deviceManager.admitDevice(invitation, secretProvider);
     }
+  }
+
+  /**
+   * Given an Invitation, return encoded JSON string invitation.
+   * @param {InvitationDescriptor} invitation
+   * @return {String}
+   */
+  encodeInvitation (invitation) {
+    return btoa(JSON.stringify(invitation.toQueryParameters()));
+  }
+
+  /**
+   * Given a encoded JSON string invitation, return an Invitation.
+   * @param {String} code an encoded JSON string invitation.
+   */
+  decodeInvitation (invitationCode) {
+    return InvitationDescriptor.fromQueryParameters(JSON.parse(atob(invitationCode)));
   }
 
   getParties () {
--- packages/react-client/src/hooks/authenticator.js
+++ packages/react-client/src/hooks/authenticator.js
@@ -2,7 +2,7 @@
 // Copyright 2020 DXOS.org
 //
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 
 import { trigger } from '@dxos/async';
 import { keyToString } from '@dxos/crypto';
@@ -17,26 +17,44 @@ import { useClient } from './client';
 export const useAuthenticator = (invitation) => {
   const client = useClient();
   const [state, setState] = useState({});
-  const [secretProvider, secretResolver] = trigger();
+  const hash = invitation ? invitation.hash : '';
+
+  // Memoize these functions by inivitation hash.
+  const [secretProvider, secretResolver] = useMemo(() => trigger(), [hash]);
 
   useEffect(() => {
+    if (!invitation) return;
+    // Use an AbortController to avoid "calling setState on unmounted component" errors.
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     async function runEffect () {
       if (invitation.identityKey) {
         // An invitation for this device to join an existing Identity.
         // Join the Identity
         await client.admitDevice(invitation, secretProvider);
-        setState({ identity: keyToString(invitation.identityKey) });
+        if (!signal.aborted) {
+          setState({ identity: keyToString(invitation.identityKey) });
+        }
       } else {
         const party = await client.joinParty(invitation, secretProvider);
-        setState({ topic: keyToString(party.publicKey) });
+        if (!signal.aborted) {
+          setState({ topic: keyToString(party.publicKey) });
+        }
       }
     }
 
     runEffect().catch(err => {
       // TODO(burdon): Doesn't support retry. Provide hint (e.g., should retry/cancel).
-      setState({ error: String(err) });
+      if (!signal.aborted) {
+        setState({ error: String(err) });
+      }
     });
-  }, []);
+
+    return () => {
+      controller.abort();
+    };
+  }, [hash]);
 
   return [state, secretResolver];
 };
