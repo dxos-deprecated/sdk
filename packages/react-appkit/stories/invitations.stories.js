71df97d69689b9caa11e42f4c7164adc984643f7
Przemyslaw Rzad <przemek@ethworks.io>
2020-08-12 17:30:43 +0200

    Improved BotDialog storybook (#149)
    * Improved BotDialog storynook
    
    * Handle async deploy in bot dialog

--- packages/react-appkit/stories/invitations.stories.js
+++ packages/react-appkit/stories/invitations.stories.js
@@ -8,11 +8,14 @@ import Box from '@material-ui/core/Box';
 import StoryRouter from 'storybook-react-router';
 import { withKnobs } from '@storybook/addon-knobs';
 
+import { sleep } from '@dxos/async';
 import { ErrorHandler } from '@dxos/debug';
 
 import { AppKitContextProvider, BotDialog } from '../src';
 import { WithClientAndIdentity, WithPartyKnobs } from './decorators';
 import { pads, NoPartyComponent } from './common';
+
+const errorHandler = new ErrorHandler();
 
 export default {
   title: 'Invitations',
@@ -21,13 +24,24 @@ export default {
 
 const BotDialogComponent = () => {
   const [open, setOpen] = useState(true);
+  const [deployed, setDeployed] = useState(false);
+
+  const handleSubmit = async ({ bot }) => {
+    await sleep(1000);
+    if (bot.includes('will-hang')) return new Promise(() => {});
+    if (bot.includes('will-fail')) throw new Error('Failed deploy');
+    setDeployed(true);
+    setOpen(false);
+  };
+
   return (
     <Box m={2}>
       <BotDialog
         open={open}
         onClose={() => setOpen(false)}
-        onSubmit={() => setOpen(false)}
+        onSubmit={handleSubmit}
       />
+      {deployed && (<p>Successfully deployed!</p>)}
     </Box>
   );
 };
@@ -35,7 +49,7 @@ const BotDialogComponent = () => {
 // TODO(burdon): Fix useRegistry to use Registry object created in context.
 export const withBotDialog = () => {
   return (
-    <AppKitContextProvider initialState={{}} errorHandler={new ErrorHandler()} pads={pads}>
+    <AppKitContextProvider initialState={{}} errorHandler={errorHandler} pads={pads}>
       <Switch>
         <Route path='/:topic' exact component={BotDialogComponent} />
         <Route path='/' exact component={NoPartyComponent} />
