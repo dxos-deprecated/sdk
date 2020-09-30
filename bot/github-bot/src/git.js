23c84ae2ae14d2b6608f8d425a5b2120709c0adc
egorgripasov <gripasovegor@gmail.com>
2020-09-29 21:52:54 -0400

    Pull function.

--- bot/github-bot/src/git.js
+++ bot/github-bot/src/git.js
@@ -4,6 +4,12 @@
 
 import Git from 'simple-git';
 
+/**
+ * @param {String} repoPath
+ * @param {String} repo
+ * @param {String} username
+ * @param {String} token
+ */
 export const cloneRepo = async (repoPath, repo, username, token) => {
   const git = Git(repoPath);
 
@@ -11,10 +17,26 @@ export const cloneRepo = async (repoPath, repo, username, token) => {
   await git.clone(fullUrl, '.');
 };
 
+/**
+ * @param {String} repoPath
+ * @param {String} message
+ */
 export const commitAndPush = async (repoPath, message = `Update ${Date.now()}`) => {
   const git = Git(repoPath);
 
   await git.add('.');
   await git.commit(message);
   await git.push();
+};
+
+/**
+ * @param {String} repoPath
+ */
+export const pullRepo = async (repoPath) => {
+  const git = Git(repoPath);
+  await git.fetch();
+  const status = await git.status();
+  if (status.behind > 0) {
+    await git.merge('origin', 'master', { '-X': 'ours' });
+  }
 };
