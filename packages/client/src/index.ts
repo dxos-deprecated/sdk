98fefbbc4053ed111dfbc05136a44d8bd861ecf5
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2020-10-21 10:30:56 +0200

    feat: react-client refactoring & stability improvements (#227)
    * Add client initialize unit test
    
    * Use ClientProvider react-client storybook
    
    * Add error message and timeout to client.initialize
    
    * Clean up storybook
    
    * Convert react-client to use ts
    
    * Lint
    
    * Use new react config
    
    * Use babelrc from echo
    
    * Remove broken microtask scheduler.
    
    * Remove broken microtask scheduler.
    
    * Remove unreachable line.
    
    * Use latest echo-db@beta
    
    * Update echo-db yarn.lock.
    
    * Export config from client
    
    Co-authored-by: Thomas E Lackey <telackey@bozemanpass.com>

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,24 +2,4 @@
 // Copyright 2020 DXOS.org
 //
 
-import queueMicrotask from 'queue-microtask';
-
-// The webpack shim process.nextTick is based on setTimeout(fn, 0). The worst thing to do.
-// This hack fix that problem for now until we find a better solution and improve the latency.
-if (typeof window !== 'undefined') {
-  process.nextTick = (...args: any[]) => {
-    const cb = args.length === 1
-      ? args[0]
-      : () => args[0](...args.slice(1));
-      
-    if (args.length === 1) {
-      if(!!window.queueMicrotask) {
-        window.queueMicrotask(cb);
-      } else {
-        queueMicrotask(cb);
-      }
-    }
-  };
-}
-
 export * from './client';
---
+++ packages/react-client/src/containers/ClientProvider.tsx
@@ -0,0 +1,25 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import React, { ReactNode } from 'react';
+
+import { Client } from '@dxos/client';
+
+import { ClientContext } from '../hooks/context';
+
+export interface ClientProviderProps {
+  client: Client
+  children?: ReactNode
+}
+
+/**
+ * Client provider container.
+ */
+export const ClientProvider = ({ client, children }: ClientProviderProps) => {
+  return (
+    <ClientContext.Provider value={{ config: client.config, client, reset: () => client.reset() }}>
+      {children}
+    </ClientContext.Provider>
+  );
+};
