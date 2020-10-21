5bac66e5569f2074d21cece948165d2bc1de5404
Dmytro Maretskyi <maretskii@gmail.com>
2020-10-21 15:09:35 +0200

    Set devtools hook in ClientProvider

--- packages/react-client/src/containers/ClientProvider.tsx
+++ packages/react-client/src/containers/ClientProvider.tsx
@@ -2,9 +2,10 @@
 // Copyright 2020 DXOS.org
 //
 
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 
 import { Client } from '@dxos/client';
+import metrics from '@dxos/metrics';
 
 import { ClientContext } from '../hooks/context';
 
@@ -17,6 +18,11 @@ export interface ClientProviderProps {
  * Client provider container.
  */
 export const ClientProvider = ({ client, children }: ClientProviderProps) => {
+
+  useEffect(() => {
+    (window as any).__DXOS__ = { client, metrics };
+  }, []);
+
   return (
     <ClientContext.Provider value={{ config: client.config, client, reset: () => client.reset() }}>
       {children}
