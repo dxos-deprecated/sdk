8bc578a63515d3369047d2d49fce4d06e592559d
rzadp <roopert7@gmail.com>
2020-10-29 14:11:16 +0100

    TS compilation for react-ux

--- packages/react-ux/jest.config.js
+++ packages/react-ux/jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  rootDir: '../..',
-  transform: {
-    '^.+/(src|dist|tests)/.+(js|jsx)$': '<rootDir>/node_modules/babel-jest'
-  },
+  preset: 'ts-jest/presets/js-with-ts',
+  coverageDirectory: 'packages/react-appkit/coverage',
   transformIgnorePatterns: [
     'node_modules/(?!(@dxos)/)'
-  ],
-  coverageDirectory: 'packages/react-ux/coverage'
+  ]
 };
