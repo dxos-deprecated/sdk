a3885ae471045429cfe4d9c53ef2278e0dde333f
HubertNastaly <h.nastaly@gmail.com>
2020-10-29 12:26:25 +0100

    Merge coverages

---
+++ packages/react-ux/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rootDir: '../..',
+  transform: {
+    '^.+/(src|dist|tests)/.+(js|jsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@dxos)/)'
+  ],
+  coverageDirectory: 'packages/react-ux/coverage'
+};
