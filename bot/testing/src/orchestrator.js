827bde10e97567ca8947ca9471e6e6161c0ac638
egorgripasov <gripasovegor@gmail.com>
2020-11-04 03:31:39 -0500

    test: Close botfactory client connection.

--- bot/testing/src/orchestrator.js
+++ bot/testing/src/orchestrator.js
@@ -62,6 +62,7 @@ export class Orchestrator {
 
   async destroy () {
     kill(this._factory.process.pid, 'SIGKILL');
+    await this._factoryClient.close();
     // TODO(egorgripasov): Produced feed store errors.
     // await this._client.destroy();
   }
