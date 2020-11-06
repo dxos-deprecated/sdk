855e6e61f174ba895054e81e64269d79c1405fc8
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2020-11-06 12:34:55 +0100

    fix: Update to new echo api (#301)
    * Update to new echo
    
    * Remove some deprecated stuff
    
    * Organize imports
    
    * fix: Update to new echo

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -5,16 +5,12 @@
 import leveljs from 'level-js';
 import memdown from 'memdown';
 
-import { Keyring, KeyStore, KeyType } from '@dxos/credentials';
+import { Keyring } from '@dxos/credentials';
 import { humanize, keyToString } from '@dxos/crypto';
-import {
-  codec, ECHO, PartyManager, PartyFactory, FeedStoreAdapter, IdentityManager, SecretProvider, InvitationOptions, InvitationDescriptor
-} from '@dxos/echo-db';
-import { SnapshotStore } from '@dxos/echo-db/dist/src/snapshot-store';
+import { ECHO, InvitationOptions, SecretProvider } from '@dxos/echo-db';
 import { FeedStore } from '@dxos/feed-store';
-import { ModelConstructor, ModelFactory } from '@dxos/model-factory';
+import { ModelConstructor } from '@dxos/model-factory';
 import { NetworkManager, SwarmProvider } from '@dxos/network-manager';
-import { ObjectModel } from '@dxos/object-model';
 import { createStorage } from '@dxos/random-access-multi-storage';
 import { raise } from '@dxos/util';
 import { Registry } from '@wirelineio/registry-client';
@@ -50,27 +46,9 @@ export interface CreateProfileOptions {
 export class Client {
   private readonly _config: ClientConfig;
 
-  private readonly _feedStore: FeedStore;
-
-  private readonly _keyring: Keyring;
-
-  private readonly _swarmConfig?: any;
-
-  private readonly _modelFactory: ModelFactory;
-
-  private readonly _identityManager: IdentityManager;
-
-  private readonly _networkManager: NetworkManager;
-
-  private readonly _partyFactory: PartyFactory;
-
-  private readonly _partyManager: PartyManager;
-
   private readonly _echo: ECHO;
 
   private readonly _registry?: any;
-
-  private readonly _snapshotStore: SnapshotStore;
 
   private _initialized = false;
 
@@ -85,35 +63,16 @@ export class Client {
       snapshotInterval
     } = config;
 
-    this._feedStore = new FeedStore(createStorage(`${storagePath}/feeds`, storageType === 'persistent' ? undefined : storageType),
-      { feedOptions: { valueEncoding: codec } });
-    this._keyring = new Keyring(new KeyStore(storageType === 'ram' ? memdown() : leveljs(`${storagePath}/keystore`)));
-    this._swarmConfig = swarm;
-
-    this._identityManager = new IdentityManager(this._keyring);
-    this._modelFactory = new ModelFactory()
-      .registerModel(ObjectModel);
-
-    this._networkManager = new NetworkManager(this._feedStore, new SwarmProvider(this._swarmConfig));
-
-    const feedStoreAdapter = new FeedStoreAdapter(this._feedStore);
-
-    this._snapshotStore = new SnapshotStore(snapshots
-      ? createStorage(`${storagePath}/snapshots`, storageType === 'persistent' ? undefined : storageType)
-      : createStorage('fake', 'ram')
-    );
-
-    this._partyFactory = new PartyFactory(
-      this._identityManager,
-      feedStoreAdapter,
-      this._modelFactory,
-      this._networkManager,
-      this._snapshotStore,
-      { snapshots, snapshotInterval }
-    );
-    this._partyManager = new PartyManager(this._identityManager, feedStoreAdapter, this._partyFactory, this._snapshotStore);
-
-    this._echo = new ECHO(this._partyManager);
+    this._echo = new ECHO({
+      feedStorage: createStorage(`${storagePath}/feeds`, storageType === 'persistent' ? undefined : storageType),
+      keyStorage: storageType === 'ram' ? memdown() : leveljs(`${storagePath}/keystore`),
+      snapshotStorage: snapshots
+        ? createStorage(`${storagePath}/snapshots`, storageType === 'persistent' ? undefined : storageType)
+        : createStorage(`${storagePath}/snapshots`, 'ram'),
+      swarmProvider: new SwarmProvider(swarm),
+      snapshots,
+      snapshotInterval
+    });
     this._registry = wns ? new Registry(wns.server, wns.chainId) : undefined;
   }
 
@@ -133,15 +92,8 @@ export class Client {
       console.error('Client.initialize is taking more then 10 seconds to complete. Something probably went wrong.');
     }, 10000);
 
-    await this._keyring.load();
+    await this._echo.open();
 
-    // If this has to be done, it should be done thru database.
-    // Actually, the we should move all initialze into database.
-    await this._partyManager.open();
-
-    if (!this._identityManager.halo && this._identityManager.identityKey) {
-      await this._partyManager.createHalo();
-    }
     this._initialized = true;
     clearInterval(timeoutId);
   }
@@ -151,7 +103,6 @@ export class Client {
    */
   async destroy () {
     await this._echo.close();
-    await this._networkManager.close();
   }
 
   /**
@@ -159,11 +110,7 @@ export class Client {
    * Warning: Inconsistent state after reset, do not continue to use this client instance.
    */
   async reset () {
-    if (this._feedStore.storage.destroy) {
-      await this._feedStore.storage.destroy();
-    }
-
-    await this._keyring.deleteAllKeyRecords();
+    await this._echo.reset();
   }
 
   /**
@@ -172,29 +119,24 @@ export class Client {
    */
   async createProfile ({ publicKey, secretKey, username }: CreateProfileOptions = {}) {
     if (publicKey && secretKey) {
-      await this._keyring.addKeyRecord({ publicKey, secretKey, type: KeyType.IDENTITY });
+      await this._echo.createIdentity({ publicKey, secretKey });
     }
 
-    if (!this._identityManager.identityKey) {
-      throw new Error('Cannot create profile. Either no keyPair (public and secret key) was provided or cannot read Identity from keyring.');
-    }
-    await this._partyManager.createHalo({
-      identityDisplayName: username || keyToString(this._identityManager.identityKey.publicKey)
-    });
+    await this._echo.createHalo(username);
   }
 
   /**
    * @returns {ProfileInfo} User profile info.
    */
   getProfile () {
-    if (!this._identityManager.identityKey) {
+    if (!this._echo.identityKey) {
       return;
     }
 
-    const publicKey = keyToString(this._identityManager.identityKey.publicKey);
+    const publicKey = keyToString(this._echo.identityKey.publicKey);
 
     return {
-      username: this._identityManager.displayName,
+      username: this._echo.identityDisplayName,
       publicKey
     };
   }
@@ -237,44 +179,6 @@ export class Client {
   }
 
   /**
-   * @deprecated
-   * Join a Party by redeeming an Invitation.
-   * @param {InvitationDescriptor} invitation
-   * @param {SecretProvider} secretProvider
-   * @returns {Promise<Party>} The now open Party.
-   */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async joinParty (invitation: InvitationDescriptor, secretProvider: SecretProvider) {
-    console.warn('deprecated. Use client.echo');
-  }
-
-  /**
-   * Redeems an invitation for this Device to be admitted to an Identity.
-   * @param {InvitationDescriptor} invitation
-   * @param {SecretProvider} secretProvider
-   * @returns {Promise<DeviceInfo>}
-   */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async admitDevice (invitation: InvitationDescriptor, secretProvider: SecretProvider) {
-    console.log('client.admitDevice: Device management is not implemented.');
-  }
-
-  /**
-   * @deprecated
-   */
-  getParties () {
-    console.warn('deprecated. Use client.echo');
-  }
-
-  /**
-   * @deprecated
-   */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  getParty (partyKey: Uint8Array) {
-    console.warn('deprecated. Use client.echo');
-  }
-
-  /**
    * Returns an Array of all known Contacts across all Parties.
    * @returns {Contact[]}
    */
@@ -285,74 +189,59 @@ export class Client {
   }
 
   /**
-   * @deprecated
-   */
-  async createSubscription () {
-    console.warn('deprecated');
-  }
-
-  /**
    * Registers a new model.
    */
   registerModel (constructor: ModelConstructor<any>): this {
-    this._modelFactory.registerModel(constructor);
+    this._echo.modelFactory.registerModel(constructor);
 
     return this;
-  }
-
-  get keyring () {
-    return this._keyring;
   }
 
   get echo () {
     return this._echo;
   }
 
-  // keep this for devtools ???
-  get feedStore () {
-    return this._feedStore;
+  get registry () {
+    return this._registry;
   }
 
-  // TODO(burdon): Remove.
+  /**
+   * For devtools.
+   *
+   * @deprecated Use echo.keyring
+   */
+  get keyring (): Keyring {
+    return this._echo.keyring;
+  }
+
+  /**
+   * For devtools.
+   *
+   * @deprecated Use echo.feedStore
+   */
+  get feedStore (): FeedStore {
+    return this._echo.feedStore;
+  }
+
+  /**
+   * For devtools.
+   *
+   * @deprecated Use echo.networkManager.
+   */
+  get networkManager (): NetworkManager {
+    return this._echo.networkManager;
+  }
+
   /**
    * @deprecated
    */
   get modelFactory () {
     console.warn('client.modelFactory is deprecated.');
-    return this._modelFactory;
-  }
-
-  // TODO(burdon): Remove.
-  /**
-   * @deprecated
-   */
-  get networkManager () {
-    return this._networkManager;
-  }
-
-  // TODO(burdon): Remove.
-  /**
-   * @deprecated
-   */
-  get partyManager () {
-    console.warn('deprecated. Use client.database');
-    return this._partyManager;
-  }
-
-  get registry () {
-    return this._registry;
+    return this._echo.modelFactory;
   }
 }
 
 const DEFAULT_SWARM_CONFIG: ClientConfig['swarm'] = {
   signal: 'ws://localhost:4000',
   ice: [{ urls: 'stun:stun.wireline.ninja:3478' }]
-};
-
-/**
- * Client factory.
- * @deprecated
- */
-export const createClient = async () => {
-  throw new Error('createClient is being deprecated. Please use new Client() instead.');
 };
