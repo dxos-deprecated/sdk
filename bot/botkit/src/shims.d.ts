f55748a3119faadcd609824ec53da2c5f8ba1654
Dmytro Maretskyi <maretskii@gmail.com>
2020-11-16 16:42:52 +0100

    Use forked version of jsondown

--- bot/botkit/src/shims.d.ts
+++ bot/botkit/src/shims.d.ts
@@ -6,4 +6,3 @@ declare module 'jsondown';
 declare module 'random-access-memory';
 declare module '@dxos/protocol-plugin-bot';
 declare module '@wirelineio/registry-client';
-declare module '@dxos/config';
