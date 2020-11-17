0d797dd7a19283f33b04aa214f097a7d4a61e099
rzadp <roopert7@gmail.com>
2020-11-17 16:40:28 +0100

    Quiet storybook in playwright

--- packages/react-appkit/scripts/start-and-wait.sh
+++ packages/react-appkit/scripts/start-and-wait.sh
@@ -7,7 +7,7 @@
 # This script waits in app on the whatever web server is used
 # Until it starts actually serving the proper index.html file
 
-yarn storybook & 
+yarn storybook --quiet & 
 set +e
 i=0
 until curl "http://localhost:9001" | grep -q "Storybook"
