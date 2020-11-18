f17999626109006fb4ca7eb4b985235ef03fd550
egorgripasov <gripasovegor@gmail.com>
2020-11-18 15:52:33 -0500

    Fix lint.

--- bot/botkit/src/bot.ts
+++ bot/botkit/src/bot.ts
@@ -217,10 +217,10 @@ export class Bot extends EventEmitter {
     return promiseTimeout(connect, CONNECT_TIMEOUT);
   }
 
-  async _startHeartbeat() {
+  async _startHeartbeat () {
     setInterval(() => {
       const used: any = process.memoryUsage();
-      for (let key in used) {
+      for (const key in used) {
         log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
       }
     }, HEARTBEAT_INTERVAL);
