a436387768077de6de415ebc01d373c1d400f694
Przemyslaw Rzad <przemek@ethworks.io>
2020-11-24 16:38:35 +0100

    fix: Device invitation flow uniform with regular invitations (#378)
    * fix: Fix an edge case with device invitation
    
    This covers an edge case when claiming device invitation, when the profile is there but `useProfile` has not updated yet.
    The 'useProfile' does update properly but the component issues a redirection already and has no chance of reacting to that.
    Using latch() around 'client.echo.joinHalo' did not help in this regard
    
    * Rename component
    
    * fix: Finished device invitation flow with uniform UX
    
    Cancelling is blocked on https://github.com/dxos/echo/issues/331
    
    * Add TS
    
    * Remove obsolete test
    
    * Add test for passcode
    
    * Remove obsolete storybooks

---
+++ packages/react-appkit/src/containers/DeviceAuthenticator.tsx
@@ -0,0 +1,141 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import React, { ReactNode, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { DialogTitle, makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Alert from '@material-ui/lab/Alert';
+
+import { InvitationDescriptor } from '@dxos/echo-db';
+import { useAuthenticator } from '@dxos/react-client';
+import { createPath, useQuery } from '@dxos/react-router';
+import { FullScreen } from '@dxos/react-ux';
+
+import { useAppRouter } from '../hooks';
+
+const useStyles = makeStyles(theme => ({
+  marginTop: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+const TitledDialog = ({ children }: {children?: ReactNode}) => {
+  return (
+    <FullScreen>
+      <Dialog open>
+        <DialogTitle>Authenticate Device</DialogTitle>
+        {children}
+      </Dialog>
+    </FullScreen>
+  );
+};
+
+/**
+ * Handles inbound invitation URL.
+ */
+const DeviceAuthenticator = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const invitation = InvitationDescriptor.fromQueryParameters(useQuery() as any);
+  // const client = useClient();
+  const appRouter = useAppRouter();
+  const [pinCode, setPinCode] = useState('');
+  const [inProgress, setInProgress] = useState(false);
+  const [cancelling, setCancelling] = useState(false);
+
+  // TODO(burdon): Provide status of remote connection (e.g., show sender key; don't display PIN until connected).
+  const [{ topic, identity, error }, setSecret] = useAuthenticator(invitation);
+  if (topic) {
+    appRouter.push({ topic });
+  } else if (identity) {
+    history.push(createPath());
+  }
+
+  const handleSubmit = async () => {
+    setInProgress(true);
+    setSecret(Buffer.from(pinCode));
+  };
+
+  const handleCancel = async () => {
+    setCancelling(true);
+    // await client.reset(); // ISSUE: https://github.com/dxos/echo/issues/331
+    window.location.replace(createPath());
+  };
+
+  const recognisedError = error &&
+    error.includes('ERR_GREET_INVALID_INVITATION') &&
+    'The invitation does not exist or the attempted access to it was unauthorized.';
+
+  if (error) {
+    return (
+      <TitledDialog>
+        <DialogContent>
+          <Alert severity="error">{recognisedError || 'System error.'}</Alert>
+          <DialogActions>
+            <Button autoFocus color='secondary' onClick={handleCancel}>Cancel</Button>
+          </DialogActions>
+        </DialogContent>
+      </TitledDialog>
+    );
+  }
+
+  if (inProgress) {
+    return (
+      <TitledDialog>
+        <DialogContent>
+          <LinearProgress />
+          <Typography className={classes.marginTop} variant='body1' gutterBottom>
+              Processing...
+          </Typography>
+        </DialogContent>
+      </TitledDialog>
+    );
+  }
+
+  return (
+    <TitledDialog>
+      <DialogContent>
+        {cancelling ? (
+          <>
+            <LinearProgress />
+            <Typography className={classes.marginTop} variant='body1' gutterBottom>
+              Cancelling...
+            </Typography>
+          </>
+        ) : (
+          <>
+            <Typography variant='body1' gutterBottom>
+            Enter the PIN number.
+            </Typography>
+            <TextField
+              value={pinCode}
+              onChange={(event) => setPinCode(event.target.value)}
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              label='PIN Code'
+              autoFocus
+            />
+          </>
+        )}
+      </DialogContent>
+      <DialogActions>
+        {/* <Button color='secondary' onClick={handleCancel} disabled={inProgress || cancelling}>Cancel</Button> */}
+        {/* ISSUE: https://github.com/dxos/echo/issues/331 */}
+        <Button color='primary' onClick={handleSubmit} disabled={inProgress || cancelling}>Submit</Button>
+      </DialogActions>
+    </TitledDialog>
+  );
+};
+
+export default DeviceAuthenticator;
