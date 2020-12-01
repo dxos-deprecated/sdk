27390109bdfbdf6d09417d0810042228e37c6d1a
HubertNastaly <h.nastaly@gmail.com>
2020-12-01 15:50:55 +0100

    Remove unused useEffect

--- packages/react-appkit/src/containers/RedeemDialog.js
+++ packages/react-appkit/src/containers/RedeemDialog.js
@@ -2,7 +2,7 @@
 // Copyright 2020 DXOS.org
 //
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
