7441a3b396a78f06c0bc635b6169c828f94f53a0
rzadp <roopert7@gmail.com>
2020-12-04 14:56:25 +0100

    Wrap up changes

--- packages/react-appkit/src/containers/PartyCardContainer.js
+++ packages/react-appkit/src/containers/PartyCardContainer.js
@@ -4,13 +4,13 @@
 
 import React, { useState } from 'react';
 
-import { keyToString, PublicKey } from '@dxos/crypto';
-import { Timeframe, schema } from '@dxos/echo-protocol';
+import { keyToString } from '@dxos/crypto';
+import { schema } from '@dxos/echo-protocol';
 import { useClient, useItems } from '@dxos/react-client';
 
 import { DefaultSettingsDialog, PartyCard } from '../components';
 import { download } from '../helpers';
-import { useAppRouter, usePads, usePartyRestore } from '../hooks';
+import { useAppRouter, usePads } from '../hooks';
 
 const PartyCardContainer = ({ party, ipfs }) => {
   const client = useClient();
@@ -20,7 +20,6 @@ const PartyCardContainer = ({ party, ipfs }) => {
   const [newItemType, setNewItemType] = useState(undefined);
   const [itemSettingsOpen, setItemSettingsOpen] = useState(false);
   const [exportInProgress, setExportInProgress] = useState(false);
-  const partyRestore = usePartyRestore(party.key, pads);
 
   const handleSavedSettings = async ({ name }, metadata = {}, callback) => {
     const pad = pads.find(p => p.type === newItemType);
@@ -40,63 +39,19 @@ const PartyCardContainer = ({ party, ipfs }) => {
     setItemSettingsOpen(true);
   };
 
-  const handleExportToIpfs = async () => {
-    // OLD behaviour
-    // const data = partyRestore.export();
-    // return ipfs.upload(data, 'text/plain');
-  };
+  const handleExportToIpfs = undefined; // TODO(rzadp): Reimplement for new ECHO
 
   const handleExportToFile = async () => {
-    // OLD behaviour
-    // const data = partyRestore.export();
-    // download(data, `${party.displayName || 'party-contents'}.json`);
-
     if (exportInProgress) {
       return;
     }
     setExportInProgress(true);
 
     const snapshot = party.database.createSnapshot();
-    console.log('snapshot', snapshot);
     const encodedSnapshot = schema.getCodecForType('dxos.echo.snapshot.DatabaseSnapshot').encode(snapshot);
-    console.log('encodedSnapshot', encodedSnapshot);
 
-    const encodedToString = encodedSnapshot.toString('hex')
-
-    // const decodedSnapshot = schema.getCodecForType('dxos.echo.snapshot.DatabaseSnapshot').decode(encodedSnapshot)
-    // console.log('decodedSnapshot', decodedSnapshot)
+    const encodedToString = encodedSnapshot.toString('hex');
     download(encodedToString, 'party-contents.txt');
-
-    // try {
-    //   const feedDescriptors = client.echo.feedStore.getDescriptors();
-    //   console.log('feedDescriptors', feedDescriptors);
-    //   const feedDescriptorsOfParty = feedDescriptors.filter(descriptor => PublicKey.equals(descriptor.metadata.partyKey, party.key));
-    //   console.log('feedDescriptorsOfParty', feedDescriptorsOfParty);
-    //   if (feedDescriptorsOfParty.length !== 1) {
-    //     throw new Error('Feed descriptor for a party not found.')
-    //   }
-    //   const result = await Promise.all(feedDescriptorsOfParty.map(descriptor => new Promise((resolve, reject) => {
-    //     descriptor.feed.getBatch(0, descriptor.feed.length, (err, data) => {
-    //       if(err) reject(err)
-    //       else {
-    //         resolve({
-    //           key: descriptor.feed.key,
-    //           data: data.filter(data => data.echo).map(data => {
-    //             // data.echo.timeframe = new Timeframe();
-    //             return data
-    //           })
-    //         })
-    //       }
-    //     })
-    //   })));
-    //   console.log('result', result)
-    // } catch (e) {
-    //   console.error('File export unsuccessful');
-    //   console.error(e.stack);
-    //   setExportInProgress(false);
-    // }
-
-    // https://github.com/hypercore-protocol/hypercore#const-id--feeddownloadrange-callback
   };
 
   const pad = newItemType ? pads.find(pad => pad.type === newItemType) : undefined;
