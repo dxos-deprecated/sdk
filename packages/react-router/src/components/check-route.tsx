422d1bf702437e1830a1e7affb06d40fa1743738
HubertNastaly <56291339+HubertNastaly@users.noreply.github.com>
2020-12-11 13:53:48 +0100

    fix: Refactor react-router to TS (#459)
    * Move files to TS
    
    * Fix router joinPaths
    
    * Detail

--- packages/react-router/src/components/check-route.tsx
+++ packages/react-router/src/components/check-route.tsx
@@ -2,6 +2,7 @@
 // Copyright 2020 DXOS.org
 //
 
+import { LocationDescriptor } from 'history';
 import queryString from 'query-string';
 import React from 'react';
 import { Redirect, useLocation, useParams } from 'react-router-dom';
@@ -10,7 +11,13 @@ import { Redirect, useLocation, useParams } from 'react-router-dom';
  * Wraps react-router Route with condition checks that may cuase a runtime redirect.
  */
 // eslint-disable-next-line jest/no-export
-export const CheckRoute = ({ children, preconditions = [] }) => {
+export const CheckRoute = ({
+  children,
+  preconditions = []
+}: {
+  children: React.ReactNode,
+  preconditions: ((args: any) => LocationDescriptor)[]
+}) => {
   const { pathname: path, search } = useLocation();
   const params = useParams();
   const query = queryString.parse(search);
