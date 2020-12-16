ef66f06cb93cf8625e4b88c1742da8caf2822fdc
HubertNastaly <h.nastaly@gmail.com>
2020-12-16 11:48:38 +0100

    Fix lint issues

--- packages/react-appkit/src/components/PartyFromFileDialog.tsx
+++ packages/react-appkit/src/components/PartyFromFileDialog.tsx
@@ -10,12 +10,11 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import PeopleIcon from '@material-ui/icons/People';
+
 import DialogHeading from './DialogHeading';
 
 const useStyles = makeStyles((theme: Theme) => ({
