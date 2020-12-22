7a264c0a955bf1c931349c24add18ab3272ee6fb
Przemyslaw Rzad <przemek@ethworks.io>
2020-12-22 11:35:15 +0100

    chore: Migrate devtools (#489)
    * Migrate devtools
    
    * Common versions
    
    * Lint versions
    
    * Fix hook usage
    
    * Lint
    
    * Versioning

---
+++ packages/devtools-extension/src/devtools-client-api/handlers/index.js
@@ -0,0 +1,19 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+import config from './config';
+import feed from './feed';
+import feedstore from './feedstore';
+import items from './items';
+import keys from './keys';
+import metrics from './metrics';
+import network from './network';
+import snapshots from './snapshots';
+import storage from './storage';
+import topic from './topic';
+
+export const initDevToolClientApi = ({ hook, bridge }) => {
+  [feed, feedstore, keys, metrics, topic, storage, items, config, snapshots, network]
+    .forEach(register => register({ hook, bridge }));
+};
